b0827be7fd1ed7b8f1a100d04c89ee7fb940e0c7
cwtshh <gucosta1719@gmail.com>
2024-08-15 17:47:51 -0300

    fix mongoconnection

--- api/config/db.js
+++ api/config/db.js
@@ -3,7 +3,7 @@ const DB_USER = process.env.MONGO_INITDB_ROOT_USERNAME;
 const DB_PASS = process.env.MONGO_INITDB_ROOT_PASSWORD;
 const DB_PORT = process.env.DB_PORT;
 const connectDB = async() => {
-    await mongoose.connect(`mongodb://${DB_USER}:${DB_PASS}@db:${DB_PORT}/`).then(res => {
+    await mongoose.connect(`mongodb://${DB_USER}:${DB_PASS}@db:${DB_PORT}/directConnection=true`).then(res => {
         console.log('Connected to MongoDB');
     }).catch(err => {
         console.log('Error connecting to MongoDB');
